358b253aa40a5575b4de7c8071d8caa7ec738990
cyperbd <x3morganx4@gmail.com>
2025-07-29 21:30:36 +0300

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <h1>MMORPG Game Placeholder</h1>
-  </React.StrictMode>
-);
